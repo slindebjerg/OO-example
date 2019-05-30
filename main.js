037fce448098eaf2ada11b304a3941660f65ee04
slindebjerg <proj.alph@gmail.com>
2019-05-30 16:11:44 +0200

    Inheritance added

--- main.js
+++ main.js
@@ -3,26 +3,31 @@ var db = {
     [
       {
         "title": "Jar of Dirt",
-        "price": "2499",
+        "price": 2499,
         "description": "Can be used to escape the evil grip of The Flying Dutchman."
       },
       {
+        "title": "Jar of Bert",
+        "price": 9,
+        "description": "Used to be part of Bert, now it's in a jar."
+      },
+      {
         "title": "Jar of Birth",
-        "price": "24999",
+        "price": 24999,
         "description": "Kinda icky."
       },
       {
         "title": "Jar of Girth",
-        "price": "499",
+        "price": 499,
         "description": "Not safe for work."
       }
     ]
 }
 
 class Store{
-  constructor(productDatabase){
+  constructor(productDatabase, cart){
     this.productArray = this.populateProductArray(productDatabase)
-    console.log(this.productArray)
+    this.cart = cart
   }
 
   populateProductArray(db){
@@ -32,6 +37,10 @@ class Store{
     })
     return productArray
   }
+
+  pushNewProductToList(newProduct){
+    this.productArray.push(newProduct)
+  }
 }
 
 class Product {
@@ -40,6 +49,36 @@ class Product {
     this.price = price;
     this.description = description;
   }
+
+  getTitle() { return this.title }
+  getPrice() { return this.price }
+  getDescription() { return this.description }
+  setTitle(newTitle) { this.title = newTitle }
+  setPrice(newPrice) { this.price = newPrice }
+  setDescription(newDescription) { this.description = newDescription }
 }
 
-store = new Store(db)
+class Cart {
+  constructor() {
+    this.productsInCart = []
+  }
+
+  pushToCart (product) {
+    this.productsInCart.push(product)
+  }
+
+  // removeFromCart (product)
+
+  getProductsInCart () {return this.productsInCart}
+}
+
+class SubProduct extends Product {
+  constructor (title, price, description, category) {
+    super(title, price, description)
+    this.category = category
+  }
+
+  getCategory () { return this.category }
+}
+
+store = new Store(db, new Cart)
